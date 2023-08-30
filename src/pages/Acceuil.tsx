1b1196bd40295a542b97e30a32012f16484a196a
ladaTvide <78384623+ladaTvide@users.noreply.github.com>
2023-08-30 10:34:33 -0400

    Ajout de la page acceuil

---
+++ src/pages/Acceuil.tsx
@@ -0,0 +1,12 @@
+import HelloWorldComponent from '../components/HelloWorldComponent';
+
+function HelloWorld() {
+    return (
+        <div>
+            <h1>Test!</h1>
+            <HelloWorldComponent/>
+        </div>
+    );
+}
+
+export default HelloWorld;
