6ba87f8c10ddd10f126d50b3568d2a980da4a0a7
Maxime V <git@maximevincent.com>
2023-08-30 10:33:38 -0400

    Ajouter classe de style

--- src/pages/HelloWorld.tsx
+++ src/pages/HelloWorld.tsx
@@ -1,9 +1,10 @@
 import HelloWorldComponent from '../components/HelloWorldComponent';
+import styles from '../styles/HelloWorld.module.css'
 
 function HelloWorld() {
     return (
         <div>
-            <h1>Hello world!</h1>
+            <h1 className={styles.h1}>Hello world!</h1>
             <HelloWorldComponent/>
         </div>
     );
