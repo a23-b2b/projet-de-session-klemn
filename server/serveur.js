e93ab7cc9006f432cdc6b89f7a45c9a5f5b653b7
Nathan Cournoyer <78099040+Nathan-Cournoyer@users.noreply.github.com>
2023-09-01 13:50:16 -0400

    Configuration des objets de logs dans server

--- server/serveur.js
+++ server/serveur.js
@@ -1,7 +1,27 @@
 const http = require("http");
 const express = require('express');
 const path = require("path");
-const logger = require("morgan");
+
+// Logger config
+const winston = require("winston");
+
+// Formatage de winston
+const formatConfig = winston.format.combine(
+    winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm' }),
+    winston.format.printf(
+      (info) => `${info.timestamp} ${info.level}: ${info.message}`
+    )
+);
+
+// Creation de l'objet de log
+const logger =  winston.createLogger({
+    level: process.env.LOG_LEVEL || 'info',
+    format: formatConfig,
+    transport: [new winston.transport.Console()]
+});
+
+
+const morgan = require("morgan");
 
 const dotenv = require('dotenv');
 dotenv.config();
