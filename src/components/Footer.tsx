520f20724e5dbecd76678e4e6e9302adca4a7080
ladaTvide <78384623+ladaTvide@users.noreply.github.com>
2023-09-01 13:06:11 -0400

    Ajustement header et footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+
+
+function Footer() {
+    return (
+        <div>
+           <h3>Footer</h3> 
+        </div>
+        
+    );
+}
+
+export default Footer;
---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+
+
+function Header() {
+    return (
+        <div>
+           <h3>Header</h3> 
+        </div>
+        
+    );
+}
+
+export default Header;
