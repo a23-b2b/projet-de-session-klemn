68d83124b1e0991e292f439a7148887808b8c69f
Maxime V <git@maximevincent.com>
2023-09-06 09:25:26 -0400

    Ajouter les deux composants sur la page de landing

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,13 +1,40 @@
+import { useState } from 'react';
 import Bonjour from '../components/ComponentWithParameters';
 import HelloWorldComponent from '../components/HelloWorldComponent';
 import LoginForm from '../components/LoginForm';
 import styles from '../styles/Landing.module.css'
+import RegisterForm from '../components/RegisterForm';
 
 function Landing() {
+    const [formIsLogin, setFormIsLogin] = useState(true);
+
+    const LoginOrRegister = () => {
+        if (formIsLogin) {
+            return (
+                <div>
+                    <LoginForm />
+                    <p onClick={() => setFormIsLogin(false)}>Vous n'avez pas de compte? Inscrivez vous maintenant!</p>
+                </div>
+            )
+        }
+
+        return (
+            <div>
+                <RegisterForm />
+                <p onClick={() => setFormIsLogin(true)}>Vous êtes déjà inscrit? Connectez vous maintenant!</p>
+            </div>
+        )
+    }
+
     return (
         <>
             <div className={styles.flexbox}>
-                <LoginForm />
+                <div>
+                    <LoginOrRegister />
+                </div>
+                <div>
+
+                </div>
             </div>
         </>
     );
