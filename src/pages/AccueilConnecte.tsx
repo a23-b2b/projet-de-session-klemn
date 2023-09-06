78cd722013e502981ec16a5f8be7e4ebd8bb35c4
ladaTvide <78384623+ladaTvide@users.noreply.github.com>
2023-09-06 09:54:41 -0400

    Ajustement header

--- src/pages/AccueilConnecte.tsx
+++ src/pages/AccueilConnecte.tsx
@@ -3,10 +3,8 @@ import styles from '../styles/AccueilConnecte.module.css'
 function AccueilConnecte() {
     return (
 
-        <div >
-            <div className={styles.h1}>
-                <h1>Test Accueil Connecté</h1>
-            </div>
+        <div id="">
+            
 
         </div>
 
