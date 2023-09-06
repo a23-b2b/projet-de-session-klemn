44fe018452d7396fbfebb97867beaf7890c69b2c
ladaTvide <78384623+ladaTvide@users.noreply.github.com>
2023-09-06 07:49:54 -0400

    Ajout de la page accueil pour les usages connectés

---
+++ src/pages/AccueilConnecte.tsx
@@ -0,0 +1,18 @@
+import styles from '../styles/AccueilConnecte.module.css'
+
+function AccueilConnecte() {
+    return (
+
+        <div className={styles.div}>
+            <div >
+                <h1>Test Acceuil Connecté</h1>
+            </div>
+
+        </div>
+
+
+
+    );
+}
+
+export default AccueilConnecte;
