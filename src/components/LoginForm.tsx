1d176ea25db47c8d791f16f958f7da499e8abcc0
Maxime V <git@maximevincent.com>
2023-09-06 09:25:14 -0400

    Utiliser la navigation pour rediriger apres etre connecte

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -3,37 +3,44 @@ import styles from '../styles/LoginRegisterForm.module.css'
 import { signInWithEmailAndPassword } from "firebase/auth"
 import { auth } from "../firebase";
 import toast from 'react-hot-toast';
-
-function loginWithEmailAndPassword(email: string, password: string) {
-    signInWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-            // Signed in 
-            const user = userCredential.user;
-            toast.success('Vous êtes connecté!')
-            // TODO: Ajouter le ID dans la BD MySQL
-        })
-        .catch((error) => {
-            console.log(error.code)
-            switch (error.code) {
-                case 'auth/user-not-found':
-                    toast.error('Le compte spécifié n\'existe pas.')
-                    break;
-                case 'auth/wrong-password':
-                    toast.error('Le mot de passe est incorrect.')
-                    break;
-                default:
-                    toast.error('Une erreur est survenue: ' + error.name)
-                    break;
-            }
-        });
-}
+import { Link, useNavigate } from "react-router-dom";
 
 function LoginForm() {
+    const navigate = useNavigate()
+
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('')
 
+    function loginWithEmailAndPassword(email: string, password: string) {
+        signInWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                // Signed in 
+                const user = userCredential.user;
+                toast.success('Vous êtes connecté!')
+                // TODO: Ajouter le ID dans la BD MySQL
+                navigate('/accueil')
+            })
+            .catch((error) => {
+                switch (error.code) {
+                    case 'auth/user-not-found':
+                        toast.error('Le compte spécifié n\'existe pas.')
+                        break;
+                    case 'auth/wrong-password':
+                        toast.error('Le mot de passe est incorrect.')
+                        break;
+                    case 'auth/invalid-email':
+                        toast.error('Le courriel est invalide.')
+                        break;
+                    default:
+                        toast.error('Une erreur est survenue: ' + error.name)
+                        break;
+                }
+            });
+    }
+
     return (
         <div>
+            <h2>Connexion</h2>
             <div className={styles.form}>
                 <label className={styles.label}>Courriel</label>
                 <input
