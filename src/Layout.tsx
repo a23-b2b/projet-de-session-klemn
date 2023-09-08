bdfedbfa989404debeb1c8e08eb7898a70abb132
Maxime V <git@maximevincent.com>
2023-09-08 14:11:32 -0400

    ajouter fichier css global

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, RouterProvider, Routes, createBrowserRouter } from "react-router-dom";
 import { Toaster } from 'react-hot-toast';
+import './styles/global.css'
 import AccueilConnecte from "./pages/AccueilConnecte";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
