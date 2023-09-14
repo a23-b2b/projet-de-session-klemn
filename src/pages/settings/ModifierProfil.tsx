0a8365b8c17774dd9d06045c9a098c5f634c5862
Maxime V <git@maximevincent.com>
2023-09-13 23:18:25 -0400

    Ajouter contenu de base page modifier profil

--- src/pages/settings/ModifierProfil.tsx
+++ src/pages/settings/ModifierProfil.tsx
@@ -5,8 +5,44 @@ import { onAuthStateChanged } from 'firebase/auth';
 import { auth } from '../../firebase';
 
 function ModifierProfil() {
+    const [firebaseLoading, setFirebaseLoading] = useState(true);
+    const [loggedIn, setLoggedIn] = useState(false);
+
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                setFirebaseLoading(false)
+                setLoggedIn(true)
+            }
+
+            else {
+                setFirebaseLoading(false)
+                setLoggedIn(false)
+            }
+        })
+    })
+    
     return (
+        <motion.div className={styles.container} initial={{ x: "-15%", opacity: 0 }} animate={{ x: "5%", opacity: 1 }}>
             <h1>Modifier Profil</h1>
+
+            <h2>Modifier le courriel</h2>
+
+            <div className={styles.form}>
+                <label className={'global_input_field_label'}>Nom d'utilisateur</label>
+                <input
+                    className={'global_input_field'}
+                    type="text"
+                // onChange={(e) => setUsername(e.target.value)} 
+                />
+                <label className={'global_input_field_label'}>Courriel</label>
+                <input
+                    className={'global_input_field'}
+                    type="text"
+                // onChange={(e) => setUsername(e.target.value)} 
+                />
+            </div>
+        </motion.div>
     );
 }
 
