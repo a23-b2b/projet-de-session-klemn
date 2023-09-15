c8c3d2a2359020647a4a20ed00f02a95972b5649
Eliefrh <2067396@bdeb.qc.ca>
2023-09-15 14:46:30 -0400

    ajout nouvelle message (aps fini )

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -1,75 +1,69 @@
 import React, { useState, useEffect } from 'react';
 import { db, messaging } from '../firebase'
-import { doc, getDoc, setDoc } from "firebase/firestore";
+import { doc, getDoc, getDocs, setDoc, addDoc, collection } from "firebase/firestore";
+
 
 
 
 function Chat() {
   const [messages, setMessages] = useState<any>(null);
+  const [messageText, setMessageText] = useState("")
 
-  const collectionName = 'messages';
+  const collectionName = 'conversation';
 
-  const documentId = 'yRzBBO1xnRv37OR66buL';
+  const documentId = 'PkqTm4Q71Xvj5yqhweMF';
+  const docRef = doc(db, collectionName, documentId);
 
 
 
 
   async function getdata() {
-    const docRef = doc(db, "conversation", documentId);
     const docSnap = await getDoc(docRef);
     //console.log(docSnap.data())
     setMessages(docSnap.data())
     //console.log(messages)
+
   }
 
-  useEffect(() => {
-    getdata();
-  })
+  // async function setdata() {
+  //   await setDoc(docRef, {
+  //     message: messageText
 
-  // const [messageData, setMessageData] = useState({});
+  //   });
 
-  // useEffect(() => {
-  //   const collectionName = 'messages';
+  // async function addData() {
+  //   await addDoc(collection(db, "messages"), {
+  //     message: messageText
+  //   });
 
-  //   const documentId = 'uARocPk3a3SABat89yYv';
+}
 
-  //   const messageDocRef = doc(db, collectionName, documentId);
 
-  //   async function fetchMessageData() {
-  //     try {
-  //       const docSnapshot = await getDoc(messageDocRef);
-  //       if (docSnapshot.exists()) {
-  //         setMessageData(docSnapshot.data());
-  //         console.log(docSnapshot.data())
-  //       } else {
-  //         console.log('Document does not exist');
-  //       }
-  //     } catch (error) {
-  //       console.error('Error fetching document:', error);
-  //     }
-  //   }
 
-  //   //fetchMessageData();
-  // }, []);
+useEffect(() => {
+  getdata();
 
-  return (
+})
+
+
+
+return (
+  <div>
+
     <div>
-
-      <div>
-        <form className="new-message-form">
-          {messages?.nom}
-          <br />
-          <input
-            className="new-message-input"
-            placeholder="Votre message..."
-          />
-          <button type="submit" className="send-button">
-            Send
-          </button>
-        </form>
-      </div>
+      {messages?.message}
+      <br />
+      <input
+        className="new-message-input"
+        placeholder="Votre message..."
+        onChange={(e) => setMessageText(e.target.value)}
+      />
+      <button className="send-button" onClick={() => addData()}>
+        Send
+      </button>
     </div>
-  );
+  </div>
+);
 }
 
 export default Chat;
