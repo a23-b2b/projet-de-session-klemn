24696df1f56422381f45cd94ccd1301f54ecb4fd
Maxime V <git@maximevincent.com>
2023-09-19 22:24:44 -0400

    Rediriger quand on publie un post

--- server/publierBlogue.js
+++ server/publierBlogue.js
@@ -35,8 +35,8 @@ module.exports = app.post('/', [body('contenu').notEmpty().isLength({max: 4000})
                     `INSERT INTO post (id_post, id_compte, id_type_post, titre, contenu, nombre_likes, nombre_dislikes,
                                        nombre_reposts, nombre_commentaires, nombre_partages, date_publication)
                      VALUES (SUBSTRING(MD5(UUID()) FROM 1 FOR 12), ?, 1, ?, ?, 0, 0, 0, 0, 0, NOW());
-                     SELECT * FROM post WHERE  id_compte=? order by date_publication desc limit 1;`,
-                    [idCompte, titre, contenu, idCompte],
+                     SELECT id_post FROM post WHERE  id_compte=? order by date_publication desc limit 1;`,
+                    [id_compte, titre, contenu, id_compte],
                     function (err, results, fields) {
                         if (err) {
                             // logger.info("Erreur lors de lexecution de la query.", err)
