d270b0e4445f14a72d61c07dca50e171e6b1fce5
Maxime V <git@maximevincent.com>
2023-09-20 10:55:30 -0400

    Utiliser scrolling infini avec  react-infinite-scroll-component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,15 +4,17 @@ import HelloWorldComponent from '../components/HelloWorldComponent';
 import styles from '../styles/Home.module.css'
 import { auth } from '../firebase';
 import { onAuthStateChanged } from 'firebase/auth';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import toast from 'react-hot-toast';
 import Post from '../components/Post';
 import BlogueForm from '../components/BlogueForm';
+import InfiniteScroll from 'react-infinite-scroll-component'
 
 function Home() {
     const navigate = useNavigate();
 
     const OFFSET = 6;
+
 
     const [postData, setPostData] = useState<any[]>([])
     const [postOffset, setPostOffset] = useState(0)
@@ -58,50 +60,52 @@ function Home() {
     return (
         <div className={styles.body}>
             <BlogueForm />
-            <div>
-                {postData?.map(({
-                    contenu,
-                    date_publication,
-                    id_compte,
-                    id_infos,
-                    id_parent,
-                    id_post,
-                    id_type_post,
-                    nombre_commentaires,
-                    nombre_dislikes,
-                    nombre_likes,
-                    nombre_partages,
-                    nombre_reposts,
-                    titre,
-                    nom_affichage,
-                    nom_utilisateur
-                }) => {
-                    return (
-                        <Post
-                            idPost={id_post}
-                            date={date_publication}
-                            nomAffichage={nom_affichage}
-                            nomUtilisateur={nom_utilisateur}
-                            titre={titre}
-                            contenu={contenu}
-                            idCompte={id_compte}
-                            nombreLike={nombre_likes}
-                            nombreDislike={nombre_dislikes}
-                            nombrePartage={nombre_partages}
-                            nombreCommentaire={nombre_commentaires}
-                            type={id_type_post}
-                            isPostFullScreen={false} />
-                    )
-                })}
-            </div>
 
-            {!isEndOfFeed && (
-                <button onClick={() => getPosts()}>Charger des posts</button>
-            )}
+            <InfiniteScroll
+                dataLength={postData.length}
+                next={() => getPosts()}
+                hasMore={!isEndOfFeed} // Replace with a condition based on your data source
+                loader={<p>Chargement...</p>}
+                endMessage={<p>No more data to load.</p>}
+            >
+                <div>
+                    {postData?.map(({
+                        contenu,
+                        date_publication,
+                        id_compte,
+                        id_infos,
+                        id_parent,
+                        id_post,
+                        id_type_post,
+                        nombre_commentaires,
+                        nombre_dislikes,
+                        nombre_likes,
+                        nombre_partages,
+                        nombre_reposts,
+                        titre,
+                        nom_affichage,
+                        nom_utilisateur
+                    }) => {
+                        return (
+                            <Post
+                                idPost={id_post}
+                                date={date_publication}
+                                nomAffichage={nom_affichage}
+                                nomUtilisateur={nom_utilisateur}
+                                titre={titre}
+                                contenu={contenu}
+                                idCompte={id_compte}
+                                nombreLike={nombre_likes}
+                                nombreDislike={nombre_dislikes}
+                                nombrePartage={nombre_partages}
+                                nombreCommentaire={nombre_commentaires}
+                                type={id_type_post}
+                                isPostFullScreen={false} />
+                        )
+                    })}
+                </div>
 
-            {isEndOfFeed && (
-                <h2>Oh non! Vous avez terminé Klemn!</h2>
-            )}
+            </InfiniteScroll>
         </div>
     );
 }
