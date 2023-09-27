3caffff4bf3047862abad019e156267fdac5f03b
Maxime V <git@maximevincent.com>
2023-09-27 12:21:30 -0400

    Ajouter action de like/dislike au click du bouton

--- src/components/Post/Footer.tsx
+++ src/components/Post/Footer.tsx
@@ -4,6 +4,8 @@ import { AiFillDislike, AiFillLike, AiOutlineShareAlt } from 'react-icons/ai';
 import { AnimatePresence } from 'framer-motion';
 import SectionReponses from '../SectionReponses';
 import { useState } from 'react';
+import { auth } from '../../firebase';
+import toast from 'react-hot-toast';
 
 interface FooterProps {
     idPost: string;
@@ -16,6 +18,33 @@ interface FooterProps {
 
 const PostFooter = (props: FooterProps) => {
     const [isReponsesOpen, setIsReponsesOpen] = useState(false);
+    const [postScore, setPostScore] = useState(props.nombreLike - props.nombreDislike)
+    const [userVote, setUserVote] = useState() // le vote de lutilisateur pour afficher le bouton en couleur ou non
+
+    function handleVote(score: number) {
+        auth.currentUser?.getIdToken(/* forceRefresh */ true)
+            .then((idToken) => {
+                fetch('http://localhost:1111/vote', {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify({
+                        user_id: auth.currentUser?.uid,
+                        post_id: props.idPost,
+                        score: score,
+                        firebase_id_token: idToken
+                    }),
+                }).then(response => response.json())
+                .then(response => {
+                    console.log('before', postScore)
+                    console.log('diff', score)
+                    console.log('result:', response['postScoreDifference'] + postScore)
+                        setPostScore(response['postScoreDifference'] + postScore)
+                    }).catch((error) => {
+                        console.log(error)
+                        toast.error('Une erreur est survenue');
+                    })
+            })
+    }
 
     return (
         <div>
@@ -27,14 +56,13 @@ const PostFooter = (props: FooterProps) => {
                 </div>
 
                 <div className={styles.like_dislike_container}>
-                    <div className={styles.bouton_interraction} id={styles.bouton_interraction_like}>
+                    <div className={styles.bouton_interraction} id={styles.bouton_interraction_like} onClick={() => handleVote(1)}>
                         <AiFillLike className={styles.icone} id={styles.icone_like} />
                     </div>
 
-                    <span className={styles.interraction_count}>{props.nombreLike - props.nombreDislike}</span>
+                    <span className={styles.interraction_count}>{postScore}</span>
 
-
-                    <div className={styles.bouton_interraction} id={styles.bouton_interraction_dislike}>
+                    <div className={styles.bouton_interraction} id={styles.bouton_interraction_dislike} onClick={() => handleVote(-1)}>
                         <AiFillDislike className={styles.icone} id={styles.icone_dislike} />
                     </div>
                 </div>
@@ -48,7 +76,7 @@ const PostFooter = (props: FooterProps) => {
 
             {!props.isPostFullScreen && (
                 <AnimatePresence>
-                    {isReponsesOpen ? <SectionReponses  idParent={props.idPost}/> : ''}
+                    {isReponsesOpen ? <SectionReponses idParent={props.idPost} /> : ''}
                 </AnimatePresence>)}
 
         </div>
