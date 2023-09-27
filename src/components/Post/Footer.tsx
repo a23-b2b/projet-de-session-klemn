d241a57681501489525975975b31a1036dc59983
Maxime V <git@maximevincent.com>
2023-09-27 17:14:54 -0400

    Animer le score qui augmente ou descend

--- src/components/Post/Footer.tsx
+++ src/components/Post/Footer.tsx
@@ -1,7 +1,7 @@
 import { BsFillReplyAllFill } from 'react-icons/bs';
 import styles from '../../styles/Post.module.css'
 import { AiFillDislike, AiFillLike, AiOutlineShareAlt } from 'react-icons/ai';
-import { AnimatePresence, useAnimate } from 'framer-motion';
+import { AnimatePresence, motion, useAnimate } from 'framer-motion';
 import SectionReponses from '../SectionReponses';
 import { useState } from 'react';
 import { auth } from '../../firebase';
@@ -21,8 +21,10 @@ const PostFooter = (props: FooterProps) => {
     const [isReponsesOpen, setIsReponsesOpen] = useState(false);
     const [postScore, setPostScore] = useState(props.nombreLike - props.nombreDislike)
     const [userVote, setUserVote] = useState(props.userVote) // le vote de lutilisateur pour afficher le bouton en couleur ou non
+    const [scoreDifference, setScoreDifference] = useState(0)
     const [scopeLike, animateLike] = useAnimate()
     const [scopeDislike, animateDisike] = useAnimate()
+    const [scopeNumberScore, animateNumberScore] = useAnimate()
 
     function handleVote(score: number) {
 
@@ -33,13 +35,13 @@ const PostFooter = (props: FooterProps) => {
                 y: '-18px',
                 rotate: Math.floor(Math.random() * 40) - 20,
             }, { duration: 0.15, ease: "anticipate" })
-            .then(() => {
-                animateLike(scopeLike.current, {
-                    scale: 1,
-                    y: '0px',
-                    rotate: 0,
-                }, { duration: 0.3, type: "spring", bounce: 0.6 })
-            })
+                .then(() => {
+                    animateLike(scopeLike.current, {
+                        scale: 1,
+                        y: '0px',
+                        rotate: 0,
+                    }, { duration: 0.3, type: "spring", bounce: 0.6 })
+                })
         }
 
         if (score < 0) {
@@ -56,29 +58,65 @@ const PostFooter = (props: FooterProps) => {
             })
         }
 
-        auth.currentUser?.getIdToken(/* forceRefresh */ true)
-            .then((idToken) => {
-                fetch('http://localhost:1111/vote', {
-                    method: 'POST',
-                    headers: { 'Content-Type': 'application/json' },
-                    body: JSON.stringify({
-                        user_id: auth.currentUser?.uid,
-                        post_id: props.idPost,
-                        score: score,
-                        firebase_id_token: idToken
-                    }),
-                }).then(response => response.json())
-                    .then(response => {
-                        // console.log('before', postScore)
-                        // console.log('diff', score)
-                        // console.log('result:', response['postScoreDifference'] + postScore)
-                        setPostScore(response['postScoreDifference'] + postScore)
-                        setUserVote(response['currentUserVote'])
-                    }).catch((error) => {
-                        console.log(error)
-                        toast.error('Une erreur est survenue');
-                    })
-            })
+        auth.currentUser?.getIdToken(/* forceRefresh */ true).then((idToken) => {
+            if (scoreDifference <= 0) {
+                animateNumberScore(scopeNumberScore.current, {
+                    y: '20px',
+                    opacity: 0,
+                }, {
+                    duration: 0.3
+                })
+            }
+
+            if (scoreDifference > 0) {
+                animateNumberScore(scopeNumberScore.current, {
+                    y: '-20px',
+                    opacity: 0,
+                }, {
+                    duration: 0.3
+                })
+            }
+
+            fetch('http://localhost:1111/vote', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    user_id: auth.currentUser?.uid,
+                    post_id: props.idPost,
+                    score: score,
+                    firebase_id_token: idToken
+                }),
+            }).then(response => response.json())
+                .then(response => {
+                    // console.log('before', postScore)
+                    // console.log('diff', score)
+                    // console.log('result:', response['postScoreDifference'] + postScore)
+                    setPostScore(response['postScoreDifference'] + postScore)
+                    setUserVote(response['currentUserVote'])
+                    setScoreDifference(response['postScoreDifference'])
+                }).catch((error) => {
+                    console.log(error)
+                    toast.error('Une erreur est survenue');
+                }).then(() => {
+                    if (scoreDifference <= 0) {
+                        animateNumberScore(scopeNumberScore.current, {
+                            y: [-20, 0],
+                            opacity: [0, 1],
+                        }, {
+                            duration: 0.3
+                        })
+                    }
+
+                    if (scoreDifference > 0) {
+                        animateNumberScore(scopeNumberScore.current, {
+                            y: [20, 0],
+                            opacity: [0, 1],
+                        }, {
+                            duration: 0.3
+                        })
+                    }
+                })
+        })
     }
 
     return (
@@ -90,22 +128,22 @@ const PostFooter = (props: FooterProps) => {
                     <span className={styles.interraction_count}>{props.nombreCommentaire}</span>
                 </div>
 
-                <div className={styles.like_dislike_container}>
-                    <div className={styles.bouton_interraction} id={styles.bouton_interraction_like} onClick={() => handleVote(1)}>
+                <motion.div className={styles.like_dislike_container} layout>
+                    <motion.div className={styles.bouton_interraction} id={styles.bouton_interraction_like} onClick={() => handleVote(1)}>
                         <div ref={scopeLike}>
                             <AiFillLike className={`styles.icone ${userVote === 1 && styles.liked_post}`} id={styles.icone_like} />
                         </div>
 
-                    </div>
+                    </motion.div>
 
-                    <span className={styles.interraction_count}>{postScore}</span>
+                    <span ref={scopeNumberScore} className={styles.interraction_count}>{postScore}</span>
 
-                    <div className={styles.bouton_interraction} id={styles.bouton_interraction_dislike} onClick={() => handleVote(-1)}>
+                    <motion.div className={styles.bouton_interraction} id={styles.bouton_interraction_dislike} onClick={() => handleVote(-1)}>
                         <div ref={scopeDislike}>
                             <AiFillDislike className={`styles.icone ${userVote === -1 && styles.disliked_post}`} id={styles.icone_dislike} />
                         </div>
-                    </div>
-                </div>
+                    </motion.div>
+                </motion.div>
 
 
                 <div className={styles.bouton_interraction} id={styles.bouton_interraction_partage}>
