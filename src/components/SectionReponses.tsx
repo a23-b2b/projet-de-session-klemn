7b8a9ad68b88d3ab30e8faf3a8a742eef81b5bb0
Maxime V <git@maximevincent.com>
2023-10-04 10:05:12 -0400

    afficher les likes de lutilisateur sur les reponses

--- src/components/SectionReponses.tsx
+++ src/components/SectionReponses.tsx
@@ -1,8 +1,10 @@
 import { motion } from 'framer-motion';
 import { useNavigate } from 'react-router-dom';
 import Post from "./Post";
-import {TYPE_REPONSE} from "./Post";
-import {useEffect, useState} from "react";
+import { TYPE_REPONSE } from "./Post";
+import { useEffect, useState } from "react";
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from '../firebase';
 
 export interface SectionReponsesProps {
     idParent: string;
@@ -14,18 +16,23 @@ function SectionReponses(props: SectionReponsesProps) {
     const [replies, setReplies] = useState<any[]>([])
 
     useEffect(() => {
-        fetch(`${process.env.REACT_APP_API_URL}/replies/${props.idParent}`, {
-            method: 'GET',
-            headers: { 'Content-Type': 'application/json' }
-        }).then(reponse => reponse.json())
-            .then(response => {
-                let data = response;
+        onAuthStateChanged(auth, (user) => {
+            fetch(`${process.env.REACT_APP_API_URL}/replies/${props.idParent}`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    authorization: user?.uid || ""
+                }
+            }).then(reponse => reponse.json())
+                .then(response => {
+                    let data = response;
 
-                setReplies(data)
-            })
-            .catch((error) => {
-                console.log(error)
-            })
+                    setReplies(data)
+                })
+                .catch((error) => {
+                    console.log(error)
+                })
+        })
     }, [])
 
     return (
@@ -33,36 +40,36 @@ function SectionReponses(props: SectionReponsesProps) {
             <motion.div initial={{ height: 0, opacity: 0 }} animate={{ height: 'auto', opacity: 1 }} exit={{ height: 0, opacity: 0 }}>
                 <h3>Commentaires</h3>
                 {replies.map(({
-                                  id_post,
-                                  id_compte,
-                                  date_publication,
-                                  nom_affichage,
-                                  nom_utilisateur,
-                                  titre,
-                                  contenu,
-                                  nombre_likes,
-                                  nombre_dislikes,
-                                  nombre_partages,
-                                  nombre_commentaires,
-                                  url_image_profil,
-                                  vote
-                              }) => {
+                    id_post,
+                    id_compte,
+                    date_publication,
+                    nom_affichage,
+                    nom_utilisateur,
+                    titre,
+                    contenu,
+                    nombre_likes,
+                    nombre_dislikes,
+                    nombre_partages,
+                    nombre_commentaires,
+                    url_image_profil,
+                    vote
+                }) => {
                     return (
                         <Post idPost={id_post}
-                        idCompte={id_compte}
-                        date={date_publication}
-                        nomAffichage={nom_affichage}
-                        nomUtilisateur={nom_utilisateur}
-                        titre={titre}
-                        contenu={contenu}
-                        nombreLike={nombre_likes}
-                        nombreDislike={nombre_dislikes}
-                        nombrePartage={nombre_partages}
-                        nombreCommentaire={nombre_commentaires}
-                        isPostFullScreen={false}
-                        type={TYPE_REPONSE} urlImageProfil={url_image_profil} 
-                        userVote={vote} />
-                        )
+                            idCompte={id_compte}
+                            date={date_publication}
+                            nomAffichage={nom_affichage}
+                            nomUtilisateur={nom_utilisateur}
+                            titre={titre}
+                            contenu={contenu}
+                            nombreLike={nombre_likes}
+                            nombreDislike={nombre_dislikes}
+                            nombrePartage={nombre_partages}
+                            nombreCommentaire={nombre_commentaires}
+                            isPostFullScreen={false}
+                            type={TYPE_REPONSE} urlImageProfil={url_image_profil}
+                            userVote={vote} />
+                    )
                 })}
             </motion.div>
         </div>
