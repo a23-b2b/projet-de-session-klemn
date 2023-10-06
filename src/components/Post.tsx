765a49c7a99dd9b10d1fa66eca37b2e2e6987fdf
Nathan Cournoyer <78099040+Nathan-Cournoyer@users.noreply.github.com>
2023-10-06 13:04:15 -0400

    Implementation du vote dans post collab et autre

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -89,7 +89,7 @@ function Post(props: Props) {
 
                     // Question Prop
                     idMeilleureReponse={props.idMeilleureReponse}
-                    statutReponse={props.statutReponse} />
+                    statutReponse={props.resolu} />
             )}
             {props.type === TYPE_COLLABORATION && (
                 <PosteCollab
@@ -109,7 +109,9 @@ function Post(props: Props) {
                     userVote={props.userVote}
 
                     // Collab Prop
-                    idCollaborateur={props.idCollaborateur}
+                    urlGit={props.urlGit}
+                    estOuvert={props.estOuvert}
+                    idCollab={props.idCollab}
                 />
             )}
         </>
