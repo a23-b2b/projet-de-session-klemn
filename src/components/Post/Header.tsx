13e8b7e59d700d397e2bf1a6fcc26ce9375bfbf7
Maxime V <git@maximevincent.com>
2023-10-09 13:52:31 -0400

    Afficher les time stamp sur le post

--- src/components/Post/Header.tsx
+++ src/components/Post/Header.tsx
@@ -8,6 +8,13 @@ interface HeaderProps {
     urlImageProfil: string;
 }
 
+
+const UNE_MINUTE_EN_SECONDES = 60
+const UNE_HEURE_EN_SECONDE = 3600
+const UN_JOUR_EN_SECONDES = 86400
+const DEUX_SEMAINES_EN_SECONDES = 1209600
+
+
 const PostHeader = (props: HeaderProps) => {
 
     const datePost = new Date(props.date)
@@ -19,6 +26,35 @@ const PostHeader = (props: HeaderProps) => {
     const dateNowSeconds = Math.round(dateNowUTC.getTime()/ 1000)
 
     const timeDifference = dateNowSeconds - datePostSeconds
+
+
+
+    let timeStampText = ""
+    // afficher en secondes
+    if (timeDifference >= 0 && timeDifference < UNE_MINUTE_EN_SECONDES) {
+        timeStampText = `${timeDifference}s`
+    }
+
+    // afficher en minutes
+    if (timeDifference >= UNE_MINUTE_EN_SECONDES && timeDifference < UNE_HEURE_EN_SECONDE) {
+        timeStampText = `${Math.round(timeDifference / 60)} min`
+    }
+
+    // afficher en heures
+    if (timeDifference >= UNE_HEURE_EN_SECONDE && timeDifference < UN_JOUR_EN_SECONDES) {
+        timeStampText = `${Math.round(timeDifference / 60 / 60)}h`
+    }
+
+    // afficher en jours
+    if (timeDifference >= UN_JOUR_EN_SECONDES && timeDifference < DEUX_SEMAINES_EN_SECONDES) {
+        timeStampText = `${Math.round(timeDifference / 60 / 60 / 60)} jours`
+    }
+
+    // la date
+    if (timeDifference >= DEUX_SEMAINES_EN_SECONDES) {
+        timeStampText = `${datePost.getDate} ${datePost.getMonth} ${datePost.getFullYear}`
+    }
+
     console.log(props.urlImageProfil)
     return (
         <div className={styles.header}>
@@ -36,7 +72,7 @@ const PostHeader = (props: HeaderProps) => {
 
             </div>
 
-            <p className={styles.date}>{props.date}</p>
+            <p className={styles.date}>{timeStampText}</p>
         </div>
     )
 }
