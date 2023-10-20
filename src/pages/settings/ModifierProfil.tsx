16c9a14e3949ea59ad21065cfcf39f18777a1ecb
Kenda04 <64617456+Kenda04@users.noreply.github.com>
2023-10-20 15:12:37 -0400

    Crop d'image fonctionnel

--- src/pages/settings/ModifierProfil.tsx
+++ src/pages/settings/ModifierProfil.tsx
@@ -1,10 +1,10 @@
-import { useEffect, useState } from 'react';
+import {useRef, useState} from 'react';
 import styles from '../../styles/ModifierProfil.module.css'
 import { motion, AnimatePresence } from "framer-motion";
 import { EmailAuthProvider, onAuthStateChanged, reauthenticateWithCredential, updateEmail, updateProfile } from 'firebase/auth';
 import { auth } from '../../firebase';
 import toast from 'react-hot-toast';
-import ReactCrop, {centerCrop, convertToPixelCrop, Crop, makeAspectCrop, PixelCrop} from "react-image-crop";
+import ReactCrop, {centerCrop, convertToPixelCrop, Crop, makeAspectCrop} from "react-image-crop";
 import 'react-image-crop/dist/ReactCrop.css'
 
 
@@ -23,6 +23,7 @@ function ModifierProfil() {
 
     const [cropProfil, setCropProfil] = useState<Crop>()
     const [urlImageProfil, setUrlImageProfil] = useState('')
+    const imgProfilRef = useRef<HTMLImageElement>()
 
     const changeEmail = () => {
 
@@ -129,7 +130,7 @@ function ModifierProfil() {
 
     function onImageProfilLoad(e: React.SyntheticEvent<HTMLImageElement, Event>) {
         // Reference: https://www.npmjs.com/package/react-image-crop
-        const { naturalWidth, naturalHeight, width, height } = e.currentTarget;
+        const { width, height } = e.currentTarget;
 
         const crop = centerCrop(
             makeAspectCrop(
@@ -145,60 +146,38 @@ function ModifierProfil() {
             height
         )
 
+        imgProfilRef.current = e.currentTarget
         setCropProfil(convertToPixelCrop(crop, width, height));
     }
 
     const changerImageProfil = () => {
-        if (cropProfil != null) {
-            let image = new Image;
-            image.src = urlImageProfil;
+        if (cropProfil && imgProfilRef.current) {
+            let image = imgProfilRef.current
 
             let canvas = document.createElement('canvas')
             const scaleX = image.naturalWidth / image.width;
             const scaleY = image.naturalHeight / image.height;
-            canvas.width = cropProfil.width
-            canvas.height = cropProfil.height
+            const pixelRatio = window.devicePixelRatio;
+            canvas.width = cropProfil.width * pixelRatio;
+            canvas.height = cropProfil.height * pixelRatio;
             const ctx = canvas.getContext('2d');
 
             if (ctx != null) {
-                // const pixelRatio = window.devicePixelRatio;
-                // canvas.width = cropProfil.width * pixelRatio;
-                // canvas.height = cropProfil.height * pixelRatio;
-                // ctx.setTransform(pixelRatio, 0, 0, pixelRatio, 0, 0);
+                ctx.setTransform(pixelRatio, 0, 0, pixelRatio, 0, 0);
                 ctx.imageSmoothingQuality = 'high';
 
                 ctx.drawImage(
                     image,
-                    cropProfil.x,
-                    cropProfil.y,
-                    image.width,
-                    image.width,
+                    cropProfil.x * scaleX,
+                    cropProfil.y * scaleY,
+                    cropProfil.width * scaleX,
+                    cropProfil.height * scaleY,
                     0,
                     0,
-                    image.width,
-                    image.height,
+                    cropProfil.width,
+                    cropProfil.height,
                 );
             }
-            // const canvas = document.createElement('canvas');
-            // const scaleX = image.naturalWidth / image.width;
-            // const scaleY = image.naturalHeight / image.height;
-            // canvas.width = 96;
-            // canvas.height = 96;
-            // const ctx = canvas.getContext('2d');
-            //
-            // if (ctx != null) {
-            //     ctx.drawImage(
-            //         image,
-            //         cropProfil.x * scaleX,
-            //         cropProfil.y * scaleY,
-            //         cropProfil.width * scaleX,
-            //         cropProfil.height * scaleY,
-            //         0,
-            //         0,
-            //         96,
-            //         96
-            //     );
-            // }
 
             console.log(cropProfil)
             console.log(canvas.toDataURL())
@@ -344,37 +323,42 @@ function ModifierProfil() {
                 </div>
 
             </div>
+
+            <br />
+            <hr className={styles.hr}></hr>
             <br />
 
-            <h3>Modifier l'image de profil</h3>
-            <div className={styles.import_image}>
-                <input
-                    type={'file'}
-                    accept={'image/'}
-                    onChange={event => {
-                        if (urlImageProfil) {
-                            URL.revokeObjectURL(urlImageProfil)
-                        }
-                        setUrlImageProfil(event.target.files ? (URL.createObjectURL(event.target.files[0])) : '')
-                    }}
-                />
-                <br/>
-                {urlImageProfil && (
-                    <ReactCrop crop={cropProfil}
-                               onChange={crop => setCropProfil(crop)}
-                               aspect={1}
-                               circularCrop={true}>
-                        <img src={urlImageProfil} onLoad={onImageProfilLoad} />
-                    </ReactCrop>
-                )
-                }
-                {urlImageProfil && (
-                    <button className={'global_bouton'} onClick={() => changerImageProfil()}>
-                    Modifier
-                    </button>
-                )
-                }
+            <div>
+                <h3 className={'global_subtitle'}>Modifier l'image de profil</h3>
+                <div className={styles.import_image}>
+                    <input
+                        type={'file'}
+                        accept={'image/'}
+                        onChange={event => {
+                            if (urlImageProfil) {
+                                URL.revokeObjectURL(urlImageProfil)
+                            }
+                            setUrlImageProfil(event.target.files ? URL.createObjectURL(event.target.files[0]) : '')
+                        }}
+                    />
+                    <br/>
+                    {urlImageProfil && (
+                        <ReactCrop crop={cropProfil}
+                                   onChange={crop => setCropProfil(crop)}
+                                   aspect={1}
+                                   circularCrop={true}>
+                            <img src={urlImageProfil} onLoad={onImageProfilLoad}/>
+                        </ReactCrop>
+                    )
+                    }
+                    {urlImageProfil && (
+                        <button className={'global_bouton'} onClick={() => changerImageProfil()}>
+                            Modifier
+                        </button>
+                    )
+                    }
 
+                </div>
             </div>
         </motion.div>
     );
