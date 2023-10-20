e567813db9d587d0ac496fdf31b07371dc521df4
ladaTvide <78384623+ladaTvide@users.noreply.github.com>
2023-10-20 15:40:21 -0400

    Ajout du CSS au modal

--- src/components/Post/QuotePostModal.tsx
+++ src/components/Post/QuotePostModal.tsx
@@ -65,8 +65,8 @@ function QuotePostModal(props: Props) {
                 <div className={styles.form}>
 
 
-                    <label className={'global_input_field_label'}>Contenu</label>
-                    <textarea className={'global_input_field'}
+
+                    <textarea className={styles.textarea}
                         rows={10}
                         maxLength={4000}
                         placeholder="Exprimez-vous!"
@@ -76,10 +76,14 @@ function QuotePostModal(props: Props) {
                             setNbCaracteres(e.target.textLength)
                         }}></textarea>
                 </div>
-                <span>{nbCaracteres}/4000</span>
-                <button className={'global_bouton'} onClick={() => publierBlogue()}>
-                    Publier
-                </button>
+                <div className={styles.conteneurDiv} id={styles["conteneurDivFooter"]}>
+
+                    <span id={styles["span"]}>{nbCaracteres}/4000</span>
+                    <button className={'global_bouton'} onClick={() => publierBlogue()}>
+                        Publier
+                    </button>
+                </div>
+
             </div>
         </Modal>
 
