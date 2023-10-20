5a8f89feb5fd2139bc91ba55bb148f93ef44564b
Maxime V <git@maximevincent.com>
2023-10-20 16:01:14 -0400

    Nouvelle requete pour créer le compte dans la BD

--- server/inscription.js
+++ server/inscription.js
@@ -2,65 +2,89 @@ const express = require('express')
 const app = express()
 const { check, body, validationResult } = require('express-validator');
 const { pool } = require('./serveur.js')
+const { admin } = require('./serveur.js')
 
 
-module.exports = app.post('/', [body('username').notEmpty(), body('email').optional().trim().isEmail()], (req, res) => {
-    const resultatValidation = validationResult(req);
-    if (resultatValidation.isEmpty()) {
+module.exports = app.post('/',
+    [
+        body('username')
+            .notEmpty()
+            .withMessage('Le pseudo ne peut pas être vide.')
+            .isLength({ min: 3, max: 254 })
+            .withMessage("Votre pseudo doit contenir entre 3 et 254 caractères")
+            .matches(/^[a-zA-Z0-9\-_]+$/)
+            .withMessage('Le pseudo contient des caractères interdits. Il doit contenir: a-Z, 0-9, -, _'),
+        body('prenom')
+            .notEmpty()
+            .withMessage('Le prénom ne peut pas être vide.')
+            .isLength({ max: 254 })
+            .withMessage("Le prénom doit faire moins de 254 caractères."),
+        body('nom')
+            .notEmpty()
+            .withMessage('Le nom ne peut pas être vide.')
+            .isLength({ max: 254 })
+            .withMessage("Le nom doit faire moins de 254 caractères."),
+    ], (req, res) => {
+        const resultatValidation = validationResult(req);
+        if (resultatValidation.isEmpty()) {
+            
+            const username = req.body.username;
+            const prenom = req.body.prenom;
+            const nom = req.body.nom;
 
-        const id_compte = req.body.id_compte;
-        const username = req.body.username;
-        const email = req.body.email;
-        const prenom = req.body.prenom;
-        const nom = req.body.nom;
-        const telephone = req.body.telephone;
+            const userToken = req.headers.authorization;
 
-        pool.query(
-            `INSERT INTO compte (
-                id_compte, 
-                date_creation_compte, 
-                nom, 
-                prenom, 
-                nom_utilisateur, 
-                courriel, 
-                telephone,
-                nombre_abonnes,
-                nombre_abonnements,
-                nom_affichage,
-                biographie,
-                url_image_profil,
-                url_image_banniere,
-                autorisation) 
+            admin.auth().verifyIdToken(userToken, true).then((payload) => {
 
-            VALUES (
-                ?, NOW(), ?, ?, ?, ?, ?, 0, 0, ?, ?, ?, ?, ?);`,
-            [
-                id_compte,
-                nom,
-                prenom,
-                username,
-                email,
-                telephone,
-                `${prenom} ${nom}`,
-                'Je viens d\'arriver sur Klemn!',
-                'http://localhost:3000/default_profile_image.jpg',
-                'http://localhost:3000/default_banner_image.webp',
-                3
-            ],
-            function (err, results, fields) {
-                if (err) {
-                    // logger.info("Erreur lors de lexecution de la query.", err)
-                    console.log(err)
-                    res.send(JSON.stringify(err))
-                }
+                const userId = payload.uid;
+                const email = payload.email;
 
-            }
-        );
+                pool.query(
+                    `INSERT INTO compte (
+                    id_compte, 
+                    date_creation_compte, 
+                    nom, 
+                    prenom, 
+                    nom_utilisateur, 
+                    courriel, 
+                    nombre_abonnes,
+                    nombre_abonnements,
+                    nom_affichage,
+                    biographie,
+                    url_image_profil,
+                    url_image_banniere,
+                    autorisation) 
+    
+                VALUES (
+                    ?, NOW(), ?, ?, ?, ?, 0, 0, ?, ?, ?, ?, ?);`,
+                    [
+                        userId,
+                        nom,
+                        prenom,
+                        username,
+                        email,
+                        username,
+                        'Je viens d\'arriver sur Klemn!',
+                        'http://localhost:3000/default_profile_image.jpg',
+                        'http://localhost:3000/default_banner_image.webp',
+                        3
+                    ],
+                    function (err, results, fields) {
+                        if (err) {
+                            // logger.info("Erreur lors de lexecution de la query.", err)
+                            console.log(err)
+                            res.send(err)
+                        }
 
-        res.status(200).send('ABOUT From about.js file')
-    } else {
-        // Erreur de validation des donnees (Express-validator)
-        // res.send({ errors: results.array() });
-        res.send(JSON.stringify(resultatValidation))
-    }
-});
+                    }
+                );
+            }).catch((error) => {
+                console.log(error)
+                res.status(500).send("ERREUR: " + error.code)
+            });
+        } else {
+            // Erreur de validation des donnees (Express-validator)
+            // res.send({ errors: results.array() });
+            res.send(resultatValidation)
+        }
+    });
