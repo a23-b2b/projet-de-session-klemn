bd07f5beb4d7bec9541f691742d0461f8bfdd8c8
Maxime V <git@maximevincent.com>
2023-10-22 16:30:43 -0400

    réactiver la validation du token sur la publication de commentaire

--- server/post/[id_post]/replies/POST_this.js
+++ server/post/[id_post]/replies/POST_this.js
@@ -25,43 +25,42 @@ export const POST_post_id_reply = app.post('/:post_id/replies', [
         return res.send(JSON.stringify(resultatValidation))
     }
 
-    // getAuth().verifyIdToken(idToken, true).then(async (payload) => {
-    // const userId = payload.uid;
-    const userId = 'a3xoaFgYWagb3Aiw4mzHi4qPi3h1'
+    getAuth().verifyIdToken(idToken, true).then(async (payload) => {
+        const userId = payload.uid;
 
-    const result = await db.insert(s.post)
-        .values({
-            id: sql`SUBSTRING(MD5(UUID()) FROM 1 FOR 12)`,
-            userId: userId,
-            parentId: postId,
-            postType: 4,
-            content: contenu,
-            likes: 0,
-            dislikes: 0,
-            reposts: 0,
-            comments: 0,
-            shares: 0,
-            createdAt: sql`NOW()`
-        });
+        const result = await db.insert(s.post)
+            .values({
+                id: sql`SUBSTRING(MD5(UUID()) FROM 1 FOR 12)`,
+                userId: userId,
+                parentId: postId,
+                postType: 4,
+                content: contenu,
+                likes: 0,
+                dislikes: 0,
+                reposts: 0,
+                comments: 0,
+                shares: 0,
+                createdAt: sql`NOW()`
+            });
 
-    console.log(result[0].affectedRows)
+        console.log(result[0].affectedRows)
 
-    if (result[0].affectedRows === 1) {
-        logger.log("info", `User '${userId}' (${req.ip}) has commented '${contenu}' on '${postId}'`)
-        return res.sendStatus(200)
-    }
+        if (result[0].affectedRows === 1) {
+            logger.log("info", `User '${userId}' (${req.ip}) has commented '${contenu}' on '${postId}'`)
+            return res.sendStatus(200)
+        }
 
-    if (!result) {
-        logger.log("error", result)
-        res.status(500).send('Erreur de base de données.')
-    }
+        if (!result) {
+            logger.log("error", result)
+            res.status(500).send('Erreur de base de données.')
+        }
 
-    if (result.length === 0) {
-        res.sendStatus(204);
-    }
+        if (result.length === 0) {
+            res.sendStatus(204);
+        }
 
-    // }).catch((error) => {
-    //     console.log(error)
-    //     return res.status(500).send("ERREUR: Le token est invalide.")
-    // });
+    }).catch((error) => {
+        console.log(error)
+        return res.status(500).send("ERREUR: Le token est invalide.")
+    });
 })
