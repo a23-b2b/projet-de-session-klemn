52aface3ec9eeecdd1e63cb7940419865ead8638
Maxime V <git@maximevincent.com>
2023-10-25 10:41:31 -0400

    Utiliser un seul form avec la creation firebase sur le serveur

--- src/components/RegisterForm.tsx
+++ src/components/RegisterForm.tsx
@@ -9,23 +9,49 @@ import { motion } from "framer-motion";
 function RegisterForm() {
     const navigate = useNavigate()
 
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('')
-    const [telephone, setTelephone] = useState('');
     const [username, setUsername] = useState('');
     const [prenom, setPrenom] = useState('');
     const [nom, setNom] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('')
+    const [passwordConfirmation, setPasswordConfirmation] = useState('')
 
-    function registerWithEmailAndPassword(email: string, password: string) {
-        createUserWithEmailAndPassword(auth, email, password)
-            .then((userCredential) => {
-                const user = userCredential.user;
-                return user;
+    function handleRegister(
+        username: string,
+        prenom: string,
+        nom: string,
+        email: string,
+        password: string,
+        passwordConfirmation: string
+    ) {
+        if (password !== passwordConfirmation) {
+            toast.error("La confirmation de mot de passe n'est pas égale au mot de passe.")
+        } else {
+            fetch(`${process.env.REACT_APP_API_URL}/user/create`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    username: username,
+                    prenom: prenom,
+                    nom: nom,
+                    email: email,
+                    password: password
+                }),
+            }).then(response => response.json()).then(response => {
+                if (response.code) throw response
+
+                toast.success('Votre profil à été créé! Vous pouvez maintenant vous connecter.');
+                navigate(0)
             }).catch((error) => {
+                console.log(error)
+
                 switch (error.code) {
                     case 'auth/invalid-email':
-                        toast.error('Le courriel est invalide.')
+                        toast.error('Le courriel est invalide.');
                         break;
+
                     case 'auth/email-already-exists':
                         toast.error('Le courriel est déjà inscrit.');
                         break;
@@ -33,14 +59,28 @@ function RegisterForm() {
                     case 'auth/weak-password':
                         toast.error('Le mot de passe est trop faible.');
                         break;
+
                     case 'auth/wrong-password':
                         toast.error('Le mot de passe est invalide. Il doit faire au moins 6 caractères.');
                         break;
+
+                    case 'ER_DUP_ENTRY':
+                        if (error.sqlMessage.endsWith("'compte.uc_compte_courriel'")) {
+                            toast.error('Le courriel est déjà inscrit.');
+                        }
+
+                        if (error.sqlMessage.endsWith("'compte.uc_compte_nom_utilisateur'")) {
+                            toast.error('Le nom d\'utilisateur est déjà pris.');
+                        }
+
+                        break;
+
                     default:
                         toast.error('Une erreur est survenue: ' + error.name)
                         break;
                 }
-            });
+            })
+        }
     }
 
     return (
@@ -54,8 +94,22 @@ function RegisterForm() {
                         className={'global_input_field'}
                         type="text"
                         onChange={(e) => setUsername(e.target.value)} />
-                    <label className={'global_input_field_label'}>Courriel</label>
 
+
+                    <label className={'global_input_field_label'}>Prénom</label>
+                    <input
+                        className={'global_input_field'}
+                        type="text"
+                        onChange={(e) => setPrenom(e.target.value)} />
+
+
+                    <label className={'global_input_field_label'}>Nom</label>
+                    <input
+                        className={'global_input_field'}
+                        type="text"
+                        onChange={(e) => setNom(e.target.value)} />
+
+                    <label className={'global_input_field_label'}>Courriel</label>
                     <input
                         className={'global_input_field'}
                         type="email"
@@ -67,28 +121,14 @@ function RegisterForm() {
                         type="password"
                         onChange={(e) => setPassword(e.target.value)} />
 
-                    <label className={'global_input_field_label'}>Nom</label>
+                    <label className={'global_input_field_label'}>Confirmez le mot de passe</label>
                     <input
                         className={'global_input_field'}
-                        type="text"
-                        onChange={(e) => setNom(e.target.value)} />
-
-
-                    <label className={'global_input_field_label'}>Prénom</label>
-                    <input
-                        className={'global_input_field'}
-                        type="text"
-                        onChange={(e) => setPrenom(e.target.value)} />
-
-
-                    <label className={'global_input_field_label'}>Numéro de téléphone</label>
-                    <input
-                        className={'global_input_field'}
-                        type="tel"
-                        onChange={(e) => setTelephone(e.target.value)} />
+                        type="password"
+                        onChange={(e) => setPasswordConfirmation(e.target.value)} />
 
                     <div className={styles.containerBouton}>
-                        <button className={'global_bouton'} onClick={() => registerWithEmailAndPassword(email, password)}>
+                        <button className={'global_bouton'} onClick={() => handleRegister(username, prenom, nom, email, password, passwordConfirmation)}>
                             Inscription
                         </button>
                     </div>
