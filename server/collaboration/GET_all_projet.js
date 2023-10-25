e388dd33e1f29faaae2a3fb35e56056a620fe221
Maxime V <git@maximevincent.com>
2023-10-25 18:34:02 -0400

    Fix bugs chargement

--- server/collaboration/GET_all_projet.js
+++ server/collaboration/GET_all_projet.js
@@ -1,38 +1,26 @@
 const express = require('express')
 const app = express()
-const mysql = require('mysql2')
 const logger = require('../logger.js')
 const { pool } = require('../serveur.js')
 
-const mysqlConnection = mysql.createConnection({
-    host: process.env.MYSQL_HOSTNAME,
-    port: process.env.MYSQL_PORT,
-    user: process.env.MYSQL_USERNAME,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE
-})
-
-module.exports = app.get('/:compte_id_proprio', (req, res) => { 
+module.exports = app.get('/:compte_id_proprio', (req, res) => {
     const id_proprio = req.params.compte_id_proprio
-    
+
     // Si accepted, faire update true + insert collaborateur
     // Sinon, faire update false
-    admin.auth().verifyIdToken(idToken, true).then((payload) => {
-        pool.query(`
+    pool.query(`
             SELECT compte_id_proprio, id_projet, titre_projet, description_projet, est_ouvert
                 FROM projet
-                WHERE compte_id_proprio = ? ;`, 
-            [id_proprio],
-            function(err, results) {
-                if (err) {
-                    res.status(500).send()
-                    logger.info(JSON.stringify(err))
-                } else if (results) {
-                    res.send(results)
-                } 
+                WHERE compte_id_proprio = ? ;`,
+        [id_proprio],
+        function (err, results) {
+            if (err) {
+                res.status(500).send()
+                logger.info(JSON.stringify(err))
+            } else if (results) {
+                res.send(results)
             }
-        )
-    }).catch((error) => {
-        res.status(500).send("ERREUR: " + error.code)
-    });
+        }
+    )
+
 })
