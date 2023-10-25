8f8718afff21479c2e53409e11748cafe5dc83da
Maxime V <git@maximevincent.com>
2023-10-25 11:27:53 -0400

    Ajouter configuration pour le push

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,6 +1,12 @@
 import type { Config } from "drizzle-kit";
+require('dotenv').config();
+
  
 export default {
-  schema: "./BD/schema.ts",
+  schema: "./BD/schema.js",
   out: "./BD/drizzle",
+  driver: 'mysql2',
+  dbCredentials: {
+    connectionString: `mysql://${process.env.MYSQL_USERNAME}:${process.env.MYSQL_PASSWORD}@${process.env.MYSQL_HOSTNAME}:${process.env.MYSQL_PORT}/${process.env.MYSQL_DATABASE}`
+  }
 } satisfies Config;
