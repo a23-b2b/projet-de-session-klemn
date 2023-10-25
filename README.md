f9bb0846f99a97d68fe454029565d6c6fb920c81
Nathan Cournoyer <78099040+Nathan-Cournoyer@users.noreply.github.com>
2023-10-25 11:00:18 -0400

    le readme est redered mais les tables ne sont pas supporte pas natif a markdown

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+
+
 Cette application a été générée avec  [Create React App](https://github.com/facebook/create-react-app).
 
 # Projet de Session Automne 2023 - Klemn
--- src/pages/AProposReadMe.tsx
+++ src/pages/AProposReadMe.tsx
@@ -1,24 +1,23 @@
 import { auth } from '../firebase';
 import { onAuthStateChanged } from 'firebase/auth';
 import toast from 'react-hot-toast';
-
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react';
 import Markdown from 'react-markdown'
-import ReactDom from 'react-dom'
 import remarkGfm from 'remark-gfm'
+import ReactDom from 'react-dom'
 
 
 function AProposReadMe () {
     // https://raw.githubusercontent.com/a23-b2b/projet-de-session-klemn/main/README.md?token=GHSAT0AAAAAACG6FDOULTCHY2ZGNYHCYGM4ZJZCVQA
     
-    const [readme, setReadme] = useState("");
-
-    useState(() => {
-        GetReadMe()
-    });
-
-    async function GetReadMe() {
-        onAuthStateChanged(auth, (user) => {
+        const [md, setMd] = useState('')
+        
+        useEffect(() => {
+            getMd()
+          }, []);
+    
+        async function getMd() {
+            onAuthStateChanged(auth, (user) => {
             if (user) {
                 user.getIdToken(/* forceRefresh */ true).then((idToken) => {
                     fetch(`${process.env.REACT_APP_API_URL}/readme`, {
@@ -29,16 +28,23 @@ function AProposReadMe () {
                         }
                     }).then(response => response.text()).then(response => {
                         console.log(response)
-                        setReadme(response)
+                        
+                        setMd( response)
                     }).catch((error) => {
                         toast.error(`Une erreur est survenue: ${error}`)
                     })
                 })
             }
-        })
-    }
+        })}
 
-    return <Markdown remarkPlugins={[remarkGfm]}>{readme}</Markdown>
+        return (<>
+            
+                <Markdown remarkPlugins={[remarkGfm]} children={md}></Markdown>
+                
+            </>)
+    
+
+   
     
 }
 
