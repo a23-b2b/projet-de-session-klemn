a043ef911eeb190d9fd0f0da969c432440ac4783
Maxime V <git@maximevincent.com>
2023-10-25 18:33:15 -0400

    utiliser le token décodé

--- server/collaboration/GET_all_demandes_collab.js
+++ server/collaboration/GET_all_demandes_collab.js
@@ -1,20 +1,14 @@
 const express = require('express')
 const app = express()
-const mysql = require('mysql2')
-const { pool } = require('../serveur.js')
+const { pool, admin } = require('../serveur.js')
 const logger = require('../logger.js')
 
-const mysqlConnection = mysql.createConnection({
-    host: process.env.MYSQL_HOSTNAME,
-    port: process.env.MYSQL_PORT,
-    user: process.env.MYSQL_USERNAME,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE
-})
-
 module.exports = app.get('/:id_compte', (req, res) => {
-    const id_compte = req.params.id_compte
+    const idToken = req.headers.authorization;
+
     admin.auth().verifyIdToken(idToken, true).then((payload) => {
+        const userId = payload.uid;
+
         pool.query(`
             SELECT c.id_compte, c.url_image_profil, c.nom_utilisateur, p.compte_id_proprio, p.id_projet, p.titre_projet, p.description_projet, id_demande_collab
                 FROM demande_collab
@@ -22,7 +16,7 @@ module.exports = app.get('/:id_compte', (req, res) => {
                 INNER JOIN projet p ON demande_collab.projet_id_projet = p.id_projet
                 WHERE p.compte_id_proprio = ? 
                 AND demande_collab.est_accepte IS NULL;`, 
-            [id_compte],
+            [userId],
             function(err, results, fields) {
                 if (err) {
                     res.status(500).send()
