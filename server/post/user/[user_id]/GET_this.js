f2569a09b3ec82ff9494252eb30aa38f269097d5
Maxime V <git@maximevincent.com>
2023-10-25 23:43:04 -0400

    Cursor navigation sur le profil

--- server/post/user/[user_id]/GET_this.js
+++ server/post/user/[user_id]/GET_this.js
@@ -3,34 +3,32 @@ const app = express()
 const { pool } = require('../../../serveur.js')
 
 
-module.exports = app.get('/user/:user_id/:offset', (req, res) => {
-    console.log(req.params)
-    const offset = parseInt(req.params.offset);
+module.exports = app.get('/user/:user_id/:cursor', (req, res) => {
+    const userCursor = parseInt(req.params.cursor);
     const userToGet = req.params.user_id;
     const limit = 6
 
     const authUserId = req.headers.authorization
 
     pool.query(`
-        SELECT post_view.*,
-            vote.id_compte AS vote_user_id,
-            vote.score
-        FROM post_view
-            LEFT JOIN vote ON post_view.id_post = vote.id_post AND post_view.id_compte = ?
-        where post_view.id_compte like ? AND post_view.id_type_post != 4
-        order by post_view.date_publication desc
-        limit ? offset ?;`,
-        [authUserId, userToGet, limit, offset],
-
+            SELECT 
+                post_view.*,
+                vote.id_compte AS vote_user_id,
+                vote.score
+            FROM post_view
+                LEFT JOIN vote ON post_view.id_post = vote.id_post AND post_view.id_compte = ?
+            WHERE post_view.numero_post > ? AND post_view.id_type_post != 4 AND post_view.id_compte like ?
+            LIMIT ?;`,
+        [authUserId, userCursor, userToGet, limit],
         function (err, results, fields) {
             if (err) {
-                // logger.info("Erreur lors de lexecution de la query GET PROFIL: ", err)
-                console.log(err)
-                res.status(500).send('Erreur de base de données', err)
+                res.status(500)
             }
             if (results) {
-                console.log(results)
-                res.status(200).send(results)
+                let nextCursor = NaN;
+                if (results.length === limit) nextCursor = results[limit - 1]['numero_post']
+
+                res.status(200).send({ "newCursor": nextCursor, "posts": [...results] })
             }
         })
 });
--- src/pages/Profil.tsx
+++ src/pages/Profil.tsx
@@ -21,7 +21,7 @@ function Profil() {
     const [visitorFollowsUser, setVisitorFollowsUser] = useState(false)
 
     const [userPosts, setUserPosts] = useState<any[]>([])
-    const [postOffset, setPostOffset] = useState(0)
+    const [cursor, setCursor] = useState(0)
     const [isEndOfFeed, setIsEndOfFeed] = useState(false)
 
 
@@ -109,24 +109,24 @@ function Profil() {
 
     function getPosts() {
         onAuthStateChanged(auth, (user) => {
-            fetch(`${process.env.REACT_APP_API_URL}/post/user/${userData.id_compte}/${postOffset}`, {
+            fetch(`${process.env.REACT_APP_API_URL}/post/user/${userData.id_compte}/${cursor}`, {
                 method: 'GET',
                 headers: {
                     'Content-Type': 'application/json',
                     'authorization': user?.uid || ''
                 }
             }).then(response => response.json()).then(response => {
-                let data = response;
+                let data = response["posts"]
 
-                console.log(data)
+                let newCursor = parseInt(response.newCursor)
 
-                setPostOffset(postOffset + OFFSET)
+                setCursor(newCursor)
 
-                if (data.length < OFFSET) {
+                if (!newCursor) {
                     setIsEndOfFeed(true)
                 }
 
-                setUserPosts(userPosts.concat(data));
+                setUserPosts(userPosts.concat(data))
             }).catch((error) => {
                 console.log(error)
             })
