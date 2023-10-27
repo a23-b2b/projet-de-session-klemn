7f2cd342840bac0d9e44aed3764bbe4f3ed96cb0
Kenda04 <64617456+Kenda04@users.noreply.github.com>
2023-10-27 12:18:39 -0400

    Progrès de la requête, blob de l'image n'est pas envoyé

--- server/user/update/POST_image_profil.js
+++ server/user/update/POST_image_profil.js
@@ -1,34 +1,45 @@
 const express = require('express')
 const app = express()
-const { check, body, validationResult } = require('express-validator');
-const { admin } = require('../../serveur.js')
-const { logger } = require('../../serveur.js')
-const { pool } = require('../../serveur.js')
-const {getStorage, ref} = require("firebase-admin/storage");
-
+const {check, body, validationResult} = require('express-validator');
+const {admin} = require('../../serveur.js')
+const {logger} = require('../../serveur.js')
+const {pool} = require('../../serveur.js')
+const {getStorage, getDownloadURL} = require("firebase-admin/storage");
 
 module.exports = app.post('/update/image_profil', (req, res) => {
-    const storage = getStorage()
+    const storageBucket = getStorage().bucket()
     const userToken = req.headers.authorization;
     const newImage = req.body.new_image;
 
+    if (!newImage || !newImage.type.includes('image/')) {
+        res.status(422).send('Le blob reçu n\'est pas une image')
+    }
 
     admin.auth().verifyIdToken(userToken, true).then((payload) => {
         const userId = payload.uid;
-
-        pool.query(
-            `UPDATE compte SET url_image_profil = ? WHERE id_compte = ?`,
-            [newImage, userId],
-            function (err, results) {
-                if (err) {
-                    // logger.info("Erreur lors de lexecution de la query GET PROFIL: ", err)
-                    res.status(500).send('Erreur de base de données', err)
-                }
-                if (results) {
-                    res.status(200).send(results)
-                }
-            });
+        const pathImage = `profil/${userId}`
+        // const imageProfilRef;
+        // getDownloadURL(imageProfilRef).then(urlImageProfil => {
+        //     console.log(urlImageProfil)
+        //     pool.query(
+        //         `UPDATE compte
+        //          SET url_image_profil = ?
+        //          WHERE id_compte = ?`,
+        //         [urlImageProfil, userId],
+        //         function (err, results) {
+        //             if (err) {
+        //                 // logger.info("Erreur lors de lexecution de la query GET PROFIL: ", err)
+        //                 console.log(err)
+        //                 res.status(500).send('Erreur de base de données')
+        //             }
+        //             if (results) {
+        //                 res.status(200).send(results)
+        //             }
+        //         });
+        // })
     }).catch((error) => {
-        res.status(500).send('Erreur de base de données', error)
+        console.log(error)
+        res.status(500).send('Erreur de base de données')
     })
+
 });
