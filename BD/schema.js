a343de80a90ae85e8795246ca6798d74abe72f7e
Maxime V <git@maximevincent.com>
2023-10-28 15:29:02 -0400

    Creer table badge

--- BD/schema.js
+++ BD/schema.js
@@ -72,6 +72,11 @@ export const question = mysqlTable("post_question", {
     answerPostId: varchar("post_meilleure_reponse", { length: 255 }).references(() => post.id),
 });
 
+export const badge = mysqlTable("badge", {
+    userId: varchar("id_compte", { length: 255 }).primaryKey().references(() => user.id),
+    badges: int("badges")
+});
+
 export const postView = mysqlView("post_view")
     .as((qb) => qb
         .select({
