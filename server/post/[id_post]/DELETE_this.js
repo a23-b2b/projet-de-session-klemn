e2a6ea7052bd1b3074dd8658a92c08c59c391c37
Kenda04 <64617456+Kenda04@users.noreply.github.com>
2023-10-30 00:00:21 -0400

    Requête pour supprimer post créée

---
+++ server/post/[id_post]/DELETE_this.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const { body, validationResult } = require('express-validator');
+const { pool } = require('../../serveur.js')
+const { admin } = require('../../serveur.js')
+
+const app = express()
+
+module.exports = app.delete('/:id_post', [body('contenu').notEmpty().isLength({ max: 4000 })], (req, res) => {
+    const resultatValidation = validationResult(req);
+    if (resultatValidation.isEmpty()) {
+
+        const postToDeleteId = req.params.id_post;
+        const idToken = req.headers.authorization;
+
+        admin.auth().verifyIdToken(idToken, true).then((payload) => {
+
+            const userId = payload.uid;
+
+            pool.query(
+                ``,
+                [userId],
+                function (err, results, fields) {
+                    if (err) {
+                        // logger.info("Erreur lors de lexecution de la query.", err)
+                        console.log(err)
+                        res.status(500).send("ERREUR: " + err.code)
+
+                    } else {
+                        res.send()
+                    }
+                }
+            );
+
+
+        }).catch((error) => {
+            res.status(500).send("ERREUR: " + error.code)
+        });
+
+    } else {
+        // Erreur de validation des donnees (Express-validator)
+        res.send(JSON.stringify(resultatValidation))
+    }
+})
