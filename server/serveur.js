15d644cfee341ff4c8f44602ffd760b882b0fafe
Maxime V <git@maximevincent.com>
2023-11-01 09:38:43 -0400

    Modifier seuil de limite

--- server/serveur.js
+++ server/serveur.js
@@ -28,8 +28,8 @@ exports.pool = pool;
 
 /* https://www.npmjs.com/package/express-rate-limit */
 const limiter = rateLimit({
-	windowMs: 15 * 60 * 1000, // 15 minutes
-	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	windowMs: 1 * 60 * 1000, // 1 minute
+	max: 90, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
 	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 })
