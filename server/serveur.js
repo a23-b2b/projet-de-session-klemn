bb86635f78fb0041aef79224fd09706f763a847a
Nathan Cournoyer <78099040+Nathan-Cournoyer@users.noreply.github.com>
2023-11-01 09:24:22 -0400

    fix dependance circulaire sur prop non-existante, il faut importer logger.js et non server pour winston logs

--- server/serveur.js
+++ server/serveur.js
@@ -26,6 +26,7 @@ const pool = mysql.createPool({
 });
 exports.pool = pool;
 
+/* https://www.npmjs.com/package/express-rate-limit */
 const limiter = rateLimit({
 	windowMs: 15 * 60 * 1000, // 15 minutes
 	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
