76633bba77cae673366f28e936475f26c99ce75c
Maxime V <git@maximevincent.com>
2023-11-24 13:24:04 -0500

    installer et configurer module passwordless.dev

---
+++ server/user/passkeys/POST_this.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express()
+const { pool } = require('../serveur.js')
+const { admin } = require('../serveur.js')
+const logger = require('../logger.js')
+const dotenv = require('dotenv')
+dotenv.config();
+
+module.exports = app.post('/new', (req, res) => {
+
+    const userToken = req.headers.authorization;
+
+    admin.auth().verifyIdToken(userToken, true).then((payload) => {
+        const userId = payload.uid;
+        const email = payload.email;
+
+        const { token } = fetch(process.env.PASSWORDLESS_API_URL + '/register/token', {
+            method: 'POST',
+            body: JSON.stringify({
+                userId: userId,
+                username: email,
+            }),
+            headers: {
+                'ApiSecret': process.env.PASSWORDLESS_PRIVATE_KEY,
+                'Content-Type': 'application/json'
+            }
+        }).then((r) => {
+            r.json()
+            console.log(r.json())
+        });
+
+        console.log(token)
+    });
+});
