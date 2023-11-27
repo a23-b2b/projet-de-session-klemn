1c40b18d6648a0dc95f579d55264256d178c824b
ladaTvide <78384623+ladaTvide@users.noreply.github.com>
2023-11-27 09:45:49 -0500

    Ajustement de la page a propos

--- src/pages/klemn.tsx
+++ src/pages/klemn.tsx
@@ -1,162 +1,210 @@
+import { useState, useEffect, useRef } from "react";
 import styles from '../styles/klemn.module.css'
 import { Link } from 'react-router-dom';
 
 
 function klemn() {
+
+
+
+    function handleClickScroll(id: any) {
+        const element = document.getElementById(id);
+        if (element) {
+            element.scrollIntoView({ behavior: 'smooth', block: 'center' });
+
+        }
+    }
+
+
+
+
+
     return (<>
         <div id={styles["conteneur"]}>
 
 
             <div id={styles["conteneurTableMatieres"]} className={'global_container_4'}>
 
-                <Link to={''}>
-                <h2>Produit</h2>
+                <Link className={'link'} to={''} onClick={() => handleClickScroll('produit')}>
+                    <h2>Produit</h2>
                 </Link>
 
-                <Link to={''}>
+                <Link className={'link'} to={''} onClick={() => handleClickScroll('aPropos')}>
                     <h3>À Propos</h3>
                 </Link>
-                
-                
-                <Link to={''}>
-                   <h3>Resources</h3> 
+
+
+                <Link className={'link'} to={''} onClick={() => handleClickScroll('resources')}>
+                    <h3>Resources</h3>
                 </Link>
 
-                <Link to={''}>
+                <Link className={'link'} to={''} onClick={() => handleClickScroll('projet')}>
                     <h3>Projet</h3>
                 </Link>
-                
-                <Link to={''}>
+
+                <Link className={'link'} to={''} onClick={() => handleClickScroll('entreprise')}>
                     <h3>Entreprise</h3>
                 </Link>
-                
+
                 <br></br>
 
-                <h2>Platformess</h2>
-                <h3>API</h3>
-                <h3>Github</h3>
-                <h3>React</h3>
-                <h3>Oracle</h3>
+
+                <Link className={'link'} to={''} onClick={() => handleClickScroll('platformes')}>
+                    <h2>Platformes</h2>
+                </Link>
+
+                <Link className={'link'} to={''} onClick={() => handleClickScroll('api')}>
+                    <h3>API</h3>
+                </Link>
+
+                <Link className={'link'} to={''} onClick={() => handleClickScroll('github')}>
+                    <h3>Github</h3>
+                </Link>
+
+                <Link className={'link'} to={''} onClick={() => handleClickScroll('react')}>
+                    <h3>React</h3>
+                </Link>
+
+                <Link className={'link'} to={''} onClick={() => handleClickScroll('oracle')}>
+                    <h3>Oracle</h3>
+                </Link>
+
                 <br></br>
 
-                <h2>Support</h2>
-                <h3>Document</h3>
-                <h3>Forum</h3>
-                <h3>Services</h3>
-                <h3>Status</h3>
+                <Link className={'link'} to={''} onClick={() => handleClickScroll('support')}>
+                    <h2>Support</h2>
+                </Link>
+
+                <Link className={'link'} to={''} onClick={() => handleClickScroll('document')}>
+                    <h3>Document</h3>
+                </Link>
+
+                <Link className={'link'} to={''} onClick={() => handleClickScroll('forum')}>
+                    <h3>Forum</h3>
+                </Link>
+
+                <Link className={'link'} to={''} onClick={() => handleClickScroll('services')}>
+                    <h3>Services</h3>
+                </Link>
+
+                <Link className={'link'} to={''} onClick={() => handleClickScroll('status')}>
+                    <h3>Status</h3>
+                </Link>
+
+
             </div>
 
             <div id={styles["conteneurPage"]}>
 
-                <h1>Produit</h1>
+                <h1 id="produit">Produit</h1>
 
-                
-                <h2>À Propos</h2>
-                <p>
-                    Explorez une multitude de possibilités sur KLEMN,
-                    où chaque section est conçue pour enrichir votre expérience.
-                    Découvrez nos produits innovants qui repoussent les limites de la collaboration en ligne.
-                    Plongez dans l'histoire et la mission de notre entreprise avec la section "À propos",
-                     et explorez les ressources qui vous aideront à maximiser votre utilisation de la plateforme.
-                </p>
-                <h2>Resources</h2>
-                <p>
-                    Explorez une multitude de possibilités sur KLEMN,
-                    où chaque section est conçue pour enrichir votre expérience.
-                    Découvrez nos produits innovants qui repoussent les limites de la collaboration en ligne.
-                    Plongez dans l'histoire et la mission de notre entreprise avec la section "À propos",
-                     et explorez les ressources qui vous aideront à maximiser votre utilisation de la plateforme.
-                </p>
-                <h2>Projet</h2>
-                <p>
-                    Explorez une multitude de possibilités sur KLEMN,
-                    où chaque section est conçue pour enrichir votre expérience.
-                    Découvrez nos produits innovants qui repoussent les limites de la collaboration en ligne.
-                    Plongez dans l'histoire et la mission de notre entreprise avec la section "À propos",
-                     et explorez les ressources qui vous aideront à maximiser votre utilisation de la plateforme.
-                </p>
-                <h2>Entreprise</h2>
-                <p>
-                    Explorez une multitude de possibilités sur KLEMN,
-                    où chaque section est conçue pour enrichir votre expérience.
-                    Découvrez nos produits innovants qui repoussent les limites de la collaboration en ligne.
-                    Plongez dans l'histoire et la mission de notre entreprise avec la section "À propos",
-                     et explorez les ressources qui vous aideront à maximiser votre utilisation de la plateforme.
-                </p>
 
-                <br></br>
-                <br></br>
-
-                <h1>Platformes</h1>
-
-                <h2>API</h2>
+                <h2 id="aPropos">À Propos</h2>
                 <p>
                     Explorez une multitude de possibilités sur KLEMN,
                     où chaque section est conçue pour enrichir votre expérience.
                     Découvrez nos produits innovants qui repoussent les limites de la collaboration en ligne.
                     Plongez dans l'histoire et la mission de notre entreprise avec la section "À propos",
-                     et explorez les ressources qui vous aideront à maximiser votre utilisation de la plateforme.
+                    et explorez les ressources qui vous aideront à maximiser votre utilisation de la plateforme.
                 </p>
-                <h2>Github</h2>
+                <h2 id="resources">Resources</h2>
                 <p>
                     Explorez une multitude de possibilités sur KLEMN,
                     où chaque section est conçue pour enrichir votre expérience.
                     Découvrez nos produits innovants qui repoussent les limites de la collaboration en ligne.
                     Plongez dans l'histoire et la mission de notre entreprise avec la section "À propos",
-                     et explorez les ressources qui vous aideront à maximiser votre utilisation de la plateforme.
+                    et explorez les ressources qui vous aideront à maximiser votre utilisation de la plateforme.
                 </p>
-                <h2>React</h2>
+                <h2 id="projet">Projet</h2>
                 <p>
                     Explorez une multitude de possibilités sur KLEMN,
                     où chaque section est conçue pour enrichir votre expérience.
                     Découvrez nos produits innovants qui repoussent les limites de la collaboration en ligne.
                     Plongez dans l'histoire et la mission de notre entreprise avec la section "À propos",
-                     et explorez les ressources qui vous aideront à maximiser votre utilisation de la plateforme.
+                    et explorez les ressources qui vous aideront à maximiser votre utilisation de la plateforme.
                 </p>
-                <h2>Oracle</h2>
+                <h2 id="entreprise">Entreprise</h2>
                 <p>
                     Explorez une multitude de possibilités sur KLEMN,
                     où chaque section est conçue pour enrichir votre expérience.
                     Découvrez nos produits innovants qui repoussent les limites de la collaboration en ligne.
                     Plongez dans l'histoire et la mission de notre entreprise avec la section "À propos",
-                     et explorez les ressources qui vous aideront à maximiser votre utilisation de la plateforme.
+                    et explorez les ressources qui vous aideront à maximiser votre utilisation de la plateforme.
                 </p>
 
                 <br></br>
                 <br></br>
-                <h1>Support</h1>
-                
-                <h2>Document</h2>
+
+                <h1 id="platformes">Platformes</h1>
+
+                <h2 id="api">API</h2>
                 <p>
                     Explorez une multitude de possibilités sur KLEMN,
                     où chaque section est conçue pour enrichir votre expérience.
                     Découvrez nos produits innovants qui repoussent les limites de la collaboration en ligne.
                     Plongez dans l'histoire et la mission de notre entreprise avec la section "À propos",
-                     et explorez les ressources qui vous aideront à maximiser votre utilisation de la plateforme.
+                    et explorez les ressources qui vous aideront à maximiser votre utilisation de la plateforme.
                 </p>
-                <h2>Forum</h2>
+                <h2 id="github">Github</h2>
                 <p>
                     Explorez une multitude de possibilités sur KLEMN,
                     où chaque section est conçue pour enrichir votre expérience.
                     Découvrez nos produits innovants qui repoussent les limites de la collaboration en ligne.
                     Plongez dans l'histoire et la mission de notre entreprise avec la section "À propos",
-                     et explorez les ressources qui vous aideront à maximiser votre utilisation de la plateforme.
+                    et explorez les ressources qui vous aideront à maximiser votre utilisation de la plateforme.
                 </p>
-                <h2>Services</h2>
+                <h2 id="react">React</h2>
                 <p>
                     Explorez une multitude de possibilités sur KLEMN,
                     où chaque section est conçue pour enrichir votre expérience.
                     Découvrez nos produits innovants qui repoussent les limites de la collaboration en ligne.
                     Plongez dans l'histoire et la mission de notre entreprise avec la section "À propos",
-                     et explorez les ressources qui vous aideront à maximiser votre utilisation de la plateforme.
+                    et explorez les ressources qui vous aideront à maximiser votre utilisation de la plateforme.
                 </p>
-                <h2>Status</h2>
+                <h2 id="oracle">Oracle</h2>
                 <p>
                     Explorez une multitude de possibilités sur KLEMN,
                     où chaque section est conçue pour enrichir votre expérience.
                     Découvrez nos produits innovants qui repoussent les limites de la collaboration en ligne.
                     Plongez dans l'histoire et la mission de notre entreprise avec la section "À propos",
-                     et explorez les ressources qui vous aideront à maximiser votre utilisation de la plateforme.
+                    et explorez les ressources qui vous aideront à maximiser votre utilisation de la plateforme.
+                </p>
+
+                <br></br>
+                <br></br>
+                <h1 id="support">Support</h1>
+
+                <h2 id="document">Document</h2>
+                <p>
+                    Explorez une multitude de possibilités sur KLEMN,
+                    où chaque section est conçue pour enrichir votre expérience.
+                    Découvrez nos produits innovants qui repoussent les limites de la collaboration en ligne.
+                    Plongez dans l'histoire et la mission de notre entreprise avec la section "À propos",
+                    et explorez les ressources qui vous aideront à maximiser votre utilisation de la plateforme.
+                </p>
+                <h2 id="forum">Forum</h2>
+                <p>
+                    Explorez une multitude de possibilités sur KLEMN,
+                    où chaque section est conçue pour enrichir votre expérience.
+                    Découvrez nos produits innovants qui repoussent les limites de la collaboration en ligne.
+                    Plongez dans l'histoire et la mission de notre entreprise avec la section "À propos",
+                    et explorez les ressources qui vous aideront à maximiser votre utilisation de la plateforme.
+                </p>
+                <h2 id="services">Services</h2>
+                <p>
+                    Explorez une multitude de possibilités sur KLEMN,
+                    où chaque section est conçue pour enrichir votre expérience.
+                    Découvrez nos produits innovants qui repoussent les limites de la collaboration en ligne.
+                    Plongez dans l'histoire et la mission de notre entreprise avec la section "À propos",
+                    et explorez les ressources qui vous aideront à maximiser votre utilisation de la plateforme.
+                </p>
+                <h2 id="status">Status</h2>
+                <p>
+                    Explorez une multitude de possibilités sur KLEMN,
+                    où chaque section est conçue pour enrichir votre expérience.
+                    Découvrez nos produits innovants qui repoussent les limites de la collaboration en ligne.
+                    Plongez dans l'histoire et la mission de notre entreprise avec la section "À propos",
+                    et explorez les ressources qui vous aideront à maximiser votre utilisation de la plateforme.
                 </p>
             </div>
         </div>
